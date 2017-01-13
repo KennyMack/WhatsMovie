f319716b7f3039ccc2d9d7c2c6bf323e7802646f
KennyMack <creativeweb.de@gmail.com>
2017-01-13 15:25:44 -0200

    form like c#

---
+++ forms/principal_design.py
@@ -0,0 +1,30 @@
+import tkinter
+from tkinter import *
+
+class frmPrincipal(object):
+    """docstring for frmPrincipal."""
+
+
+    def __init__(self):
+        super(frmPrincipal, self).__init__()
+        self.title = 'Inicial'
+        self.form = tkinter.Tk()
+        self.size = '640x480'
+        self.icon = 'favicon.ico'
+        self.lblWhichMovie = Label()
+        self.txtWhichMovie = Entry()
+        self.btnSearch = Button()
+
+        self.form.title(self.title)
+        self.form.geometry(self.size)
+        self.form.wm_iconbitmap(self.icon)
+        self.lblWhichMovie = Label(self.form, text='Qual filme deseja ?')
+        self.lblWhichMovie.pack(anchor=NW, padx=20, pady=5)
+        self.txtWhichMovie = Entry(self.form)
+        self.txtWhichMovie.pack(anchor=NW, padx=20, fill=X, pady=2)
+        self.btnSearch = Button(self.form)
+        self.btnSearch.text = 'Busca'
+        self.btnSearch.pack(side=TOP, fill=X, padx=20, pady=15)
+
+    def show(self):
+        self.form.mainloop()
--- main.py
+++ main.py
@@ -7,17 +7,20 @@ from io import BytesIO
 from urllib.request import urlopen
 import io
 import base64
+from forms.principal_design import frmPrincipal
 
 
 def main():
-    window = tkinter.Tk()
+    frm = frmPrincipal()
+    frm.show()
+    """window = tkinter.Tk()
     window.title('Inicial')
     window.geometry("640x480")
     window.wm_iconbitmap('favicon.ico')
     Label(window, text='Qual filme deseja ?').pack(anchor=NW, padx=20, pady=5)
     Entry(window).pack(anchor=NW, padx=20, fill=X, pady=2)
     Button(window, text='Buscar').pack(side=TOP, fill=X, padx=20, pady=15)
-    window.mainloop()
+    window.mainloop()"""
 
 
 if __name__ == '__main__':
